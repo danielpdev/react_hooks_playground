e52474b9b991fea5ff2049fd032f4c4def9f1909
danielpdev <popa.daniel19@gmail.com>
2018-11-13 16:50:08 +0200

    changed home message

--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ import "./styles.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSanxdbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>Magic hooks</h2>
       <StateHook />
     </div>
   );
